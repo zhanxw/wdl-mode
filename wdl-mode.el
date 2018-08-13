a9e6f8b407811d3cdf46df954855f1e642fcdff6
Philip Lijnzaad <plijnzaad@gmail.com>
2018-08-13 16:00:11 +0200

    installation instructions

--- wdl-mode.el
+++ wdl-mode.el
@@ -14,6 +14,25 @@
 
 ;; This package provides a major mode for WDL (Workflow Definition Language).
 ;; It supports basic font-lock highlights and indentation.
+;;
+;; Installation instructions:
+;;
+;;   $ cd YOUR_GIT_DIRECTORY
+;;   $ git clone https://github.com/plijnzaad/wdl-mode.git
+;;
+;; and add EITHER the following line to your .emacs file
+;;     (require 'wdl-mode "YOUR_GIT_DIRECTORY/wdl-mode/wdl-mode.el" t)
+;;
+;; to always load the mode, OR make it autoload by adding the following:
+;;
+;;     (add-to-list 'auto-mode-alist '("\\.wdl\\'" . wdl-mode))
+;;     (autoload 'wdl-mode YOUR_GIT_DIRECTORY/wdl-mode/wdl-mode.el" nil t)
+;;
+;; and execute it (or restart emacs). To adjust the indentation setting
+;; add something like this to your .emacs: 
+;;
+;;     (setq wdl-indent-level 4)
+;;
 
 ;;; Code:
 
