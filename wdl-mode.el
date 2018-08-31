cef86e5afc136ae5ad9324cd6e6d6f860b889bcf
zhanxw <zhanxw@users.noreply.github.com>
2018-08-31 14:46:13 -0500

    Update wdl-mode.el

--- wdl-mode.el
+++ wdl-mode.el
@@ -17,8 +17,18 @@
 ;;
 ;; Installation instructions:
 ;;
+;;   Use MELPA (easier)
+;;
+;;     1. Install MELPA (https://melpa.org/#/getting-started)
+;;     2. In Emacs, M-x package-list-package, 
+;;        select `wdl-mode`, 
+;         press `i` to mark it for installation, 
+;;        then press `x` to perform installation.
+;;
+;;   Manual installation
+;; 
 ;;   $ cd YOUR_GIT_DIRECTORY
-;;   $ git clone https://github.com/plijnzaad/wdl-mode.git
+;;   $ git clone https://github.com/zhanxw/wdl-mode.git
 ;;
 ;; and add EITHER the following line to your .emacs file
 ;;     (require 'wdl-mode "YOUR_GIT_DIRECTORY/wdl-mode/wdl-mode.el" t)
@@ -28,8 +38,10 @@
 ;;     (add-to-list 'auto-mode-alist '("\\.wdl\\'" . wdl-mode))
 ;;     (autoload 'wdl-mode YOUR_GIT_DIRECTORY/wdl-mode/wdl-mode.el" nil t)
 ;;
-;; and execute it (or restart emacs). To adjust the indentation setting
-;; add something like this to your .emacs: 
+;; and execute it (or restart emacs). 
+;; 
+;; Customization:
+;;     to customize indentaiton, add something like this to your .emacs: 
 ;;
 ;;     (setq wdl-indent-level 4)
 ;;
@@ -51,10 +63,11 @@
         (modify-syntax-entry ?_ "w" synTable)  ;; treat a_b as part of the word
         synTable))
 
-(setq wdl-keywords
-      '("call" "runtime" "task" "workflow" "if" "then" "else" "import" "as" "input" "output" "meta" "parameter_meta" "scatter" "command"))
+(defconst wdl-keywords
+      '("call" "runtime" "task" "workflow" "if" "then" "else" "import" "as" "input" "output" "meta" "parameter_meta" "scatter" "command")
+      "Syntax table for `wdl-mode'.")
 
-(setq wdl-font-lock-keywords
+(defconst wdl-font-lock-keywords
       (let* (
              ;; define several categories
              (x-keywords wdl-keywords)
@@ -146,7 +159,7 @@
 ;;;###autoload
 (add-to-list 'auto-mode-alist '("\\.wdl\\'" . wdl-mode))
 
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; ;; useless codes
 ;; (defconst demo-triple-quoted-string-regex
 ;;   (rx "\"\"\""))
